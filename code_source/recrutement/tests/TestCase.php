e6c87fa7d61b7c40549677ce674e9394e919df6b
ItokianaRdrsn <anjaitokianaran@gmail.com>
2026-08-25 09:07:22 +0300

    commit

--- code_source/recrutement/tests/TestCase.php
+++ code_source/recrutement/tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $this->traitsUsedByTest = class_uses_recursive(static::class);
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
